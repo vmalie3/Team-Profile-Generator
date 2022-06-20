2f37f5f8dd27307fbe2fca0c428a2e8fc8cb9f89
Victoria Malie <vmalie3@gmail.com>
2022-06-20 12:08:55 -0500

    finally got it to create team array

--- index.js
+++ index.js
@@ -2,19 +2,17 @@
 //Engineer: name, ID, email, GitHub username
 //Intern: name, ID, email, school
 const inquirer = require('inquirer');
+inquirer.registerPrompt('loop', require('inquirer-loop')(inquirer))
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const Manager = require('./lib/manager');
 
-
+const teamArr = [];
 
 const questions = [
     {
-        type: 'rawlist',
-        message: 'What is the employee\'s title?',
-        name:'title',
-        choices: ['Manager', 'Engineer', "Intern"]
-    },
-    {
         type: 'input',
-        message: 'What is the employee\'s name?',
+        message: 'What is the manager\'s name?',
         name: 'name',
         validate: name => {
             if (name) {
@@ -27,7 +25,7 @@ const questions = [
     },
     {
         type: 'input',
-        message: 'What is the employee\'s ID?',
+        message: 'What is the manager\'s ID?',
         name: 'id',
         validate: id => {
             if (id) {
@@ -40,7 +38,7 @@ const questions = [
     },
     {
         type: 'input',
-        message: 'What is the employee\'s email?',
+        message: 'What is the manager\'s email?',
         name: 'email',
         validate: email => {
             if (email) {
@@ -57,31 +55,98 @@ const questions = [
         name: 'officeNumber'
     },
     {
-        type: 'input',
-        message: 'What is the engineer\'s GitHub username?',
-        name: 'gitHub'
-    },
-    {
-        type: 'input',
-        message: 'What school does this intern attend?',
-        name: 'school'
-    },
-    {
-        type: 'confirm',
-        message: 'Would you like to add another employee?',
-        name: 'addAnother'
-    },
+    type: 'loop',
+    name: 'addEmployee',
+    message: 'Do you want to add another employee to the team?',
+    questions: [
+        {
+            type: 'rawlist',
+            message: 'What is the employee\'s title?',
+            name:'title',
+            choices: ['Engineer', "Intern"]
+        },
+        {
+            type: 'input',
+            message: 'What is the employee\'s name?',
+            name: 'name',
+            validate: name => {
+                if (name) {
+                    return true;
+                } else {
+                    console.log('Enter the employee\'s name before continuing.')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'What is the employee\'s ID?',
+            name: 'id',
+            validate: id => {
+                if (id) {
+                    return true;
+                } else {
+                    console.log('Enter the employee\'s name before continuing.')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'What is the employee\'s email?',
+            name: 'email',
+            validate: email => {
+                if (email) {
+                    return true;
+                } else {
+                    console.log('Enter the employee\'s name before continuing.')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'What is the engineer\'s GitHub username?',
+            name: 'gitHub',
+            when: (addEmployee) => addEmployee.title === 'Engineer'
+        },
+        {
+            type: 'input',
+            message: 'What school does this intern attend?',
+            name: 'school',
+            when: (addEmployee) => addEmployee.title === 'Intern'
+        },
+    ]}
 ]
+
 
 function addEmployee() {
     inquirer.prompt(questions)
     .then(data => {
-        let {title, name, id, email, officeNumber, gitHub, school, addAnother} = data;
 
-        if (addAnother) {
-            addEmployee();
-        } else {
-            console.log('Your team has been successfully created!')
+        let {name, id, email, officeNumber, addEmployee} = data;
+
+        const manager = new Manager (name, id, email, officeNumber);
+        teamArr.push(manager);
+        const employees = addEmployee;
+        let newEmployee;
+
+        for (let i = 0; i < employees.length; i++) {
+            if (employees[i].title === 'Engineer') {
+                let {name, id, email, gitHub} = employees[i];
+                newEmployee = new Engineer (name, id, email, gitHub);
+                teamArr.push(newEmployee);
+            } else if (employees[i].title === 'Intern') {
+                let {name, id, email, school} = employees[i];
+                newEmployee = new Intern (name, id, email, school);
+                teamArr.push(newEmployee);
+            }
         }
+
+    
+        
+        console.log(teamArr);
     })
 }
+
+addEmployee();
